9aa5ed3169aa8729d5e563e1748f5456c203d08f
StevenAwo <rotimiawomodu@gmail.com>
2024-06-16 16:41:50 +0100

    TASK 1

---
+++ utils/db.js
@@ -0,0 +1,55 @@
+import { MongoClient } from 'mongodb';
+
+const HOST = process.env.DB_HOST || 'localhost';
+
+const PORT = process.env.DB_PORT || 27017;
+
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+const url = `mongodb://${HOST}:${PORT}`;
+
+class DBClient {
+  constructor() {
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+
+    }).catch((err) => {
+
+      console.log(err);
+
+    });
+  }
+
+  isAlive() {
+
+    return this.client.isConnected();
+
+  }
+
+  async nbUsers() {
+
+    const userrs = this.db.collection('users');
+
+    const userrsNumb = await userrs.countDocuments();
+
+    return userrsNumb;
+
+  }
+
+  async nbFiles() {
+
+    const filles = this.db.collection('files');
+
+    const fillesNumb = await filles.countDocuments();
+
+    return fillesNumb;
+
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
